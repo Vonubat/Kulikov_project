8596c5947f66edcd51e3578fd1f850daded2ed23
Tabunov Egor <Vonubat@gmail.com>
2022-07-19 03:37:38 +0300

    feat: add slider

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
 import './styles/normalize.css';
 import './styles/main.scss';
 import 'bootstrap';
+import Swiper from 'swiper';
+
+new Swiper('.mySwiper', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
