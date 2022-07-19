0b9fa33d54b21bc5ef05791916e09218048a73fd
Tabunov Egor <Vonubat@gmail.com>
2022-07-19 05:58:43 +0300

    refactor: disable unusable modules

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import './styles/base/normalize.css';
 import './styles/styles.scss';
-import 'bootstrap';
-import Swiper from 'swiper/bundle';
+import Swiper, { Navigation, Pagination, Autoplay } from 'swiper';
 
 new Swiper('.slider', {
   slidesPerView: 3,
   spaceBetween: 30,
   freeMode: true,
+  modules: [Navigation, Pagination, Autoplay],
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
