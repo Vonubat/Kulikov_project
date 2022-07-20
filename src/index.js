1de2df0835e362524711a8e9578fed71dc460234
Tabunov Egor <Vonubat@gmail.com>
2022-07-20 08:22:30 +0300

    feat: deploy the application

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import 'bootstrap';
-import './styles/styles.scss';
-import './components/view/slider';
-import './components/view/services';
-import './components/view/header';
-import './components/view/modal';
